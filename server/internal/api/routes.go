24d25b581fc4e8bb413df35059494f8ae2297108
Elias-Larsson <aroezi05@gmail.com>
2026-05-23 18:41:17 +0200

    add workflowrun handler

--- server/internal/api/routes.go
+++ server/internal/api/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"context"
+	"monad/internal/handlers"
 	"time"
 
 	"github.com/gofiber/fiber/v3"
@@ -9,13 +10,20 @@ import (
 )
 
 func Setup(app *fiber.App, pool *pgxpool.Pool) {
-	api := app.Group("/api")
 
-	api.Get("/health", func(c fiber.Ctx) error {
+	app.Get("/health", func(c fiber.Ctx) error {
 		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 		defer cancel()
 		if err := pool.Ping(ctx); err != nil {
 			return c.Status(fiber.StatusServiceUnavailable).SendString("db unavailable")
+		}
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Post("/workflow/run", func(c fiber.Ctx, pool *pgxpool.Pool) error {
+		err := handlers.WorkflowRun(c, pool)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 		return c.SendStatus(fiber.StatusOK)
 	})
---
+++ server/internal/handlers/workflow-handler.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+
+	"monad/internal/queue"
+	"monad/internal/workflow"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func WorkflowRun(c fiber.Ctx, pool *pgxpool.Pool) error {
+	var r workflow.WorkflowRun
+
+	// Parse request
+	if err := json.Unmarshal(c.Body(), &r); err != nil {
+		return c.Status(fiber.StatusBadRequest).
+			SendString(err.Error())
+	}
+
+	// Generate IDs
+	runID := uuid.New().String()
+
+	// Insert workflow run
+	_, err := pool.Exec(
+		context.Background(),
+		`
+		INSERT INTO workflow_runs (
+			id,
+			workflow_id,
+			status
+		)
+		VALUES ($1, $2, $3)
+		`,
+		runID,
+		r.WorkflowID,
+		"PENDING",
+	)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).
+			SendString(err.Error())
+	}
+
+	// Publish message to RabbitMQ
+	msg := map[string]any{
+		"workflow_run_id": runID,
+		"workflow_id":     r.WorkflowID,
+		"status":          "PENDING",
+	}
+
+	body, _ := json.Marshal(msg)
+
+	if err := queue.Publish(body); err != nil {
+		return c.Status(fiber.StatusInternalServerError).
+			SendString(err.Error())
+	}
+
+	return c.JSON(fiber.Map{
+		"workflow_run_id": runID,
+		"status":          "started",
+	})
+}
