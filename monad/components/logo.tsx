24712092c119e30a34467c4cc7eb4c82d7ab5d24
Elias-Larsson <aroezi05@gmail.com>
2026-07-01 22:11:50 +0200

    fix Logo animation and eslint

--- monad/components/logo.tsx
+++ monad/components/logo.tsx
@@ -2,12 +2,14 @@ import styles from "./logo.module.css";
 
 export const Logo = () => {
   return (
-        <div className={styles.monadLogo}>
-        <span className={styles.m}>M</span>
-        <span className={styles.oTrack}>
-          <span className={styles.o}>o</span>
-        </span>
-          <span className={styles.tail}>nad</span>
-        </div>
+    <div className={styles.monadLogo}>
+      <span className={styles.m}>M</span>
+      <span className={styles.oTrack}>
+        <span className={styles.o}>o</span>
+      </span>
+      <span className={styles.tailWrap}>
+        <span className={styles.tail}>nad</span>
+      </span>
+    </div>
   );
-}
+};
